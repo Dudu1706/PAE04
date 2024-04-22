ac9be1e1acea1d37328c906baceaeddd3a46c65f
Eduardo Aguiar Leite da Silva <dudu170606@gmail.com>
2024-04-21 22:09:54 -0300

    Adicionei os nomes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/app.dart';
-
+// RA: 24.00380-8. Eduardo Aguiar Leite da Silva
+// RA: 24.00947-4. Victor Hugo Gaetano Oliveira Tavares de Pinho
 main() {
   runApp(const App());
 }
